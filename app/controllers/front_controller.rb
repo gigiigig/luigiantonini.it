2f3126b00e51675923973f29be5ea74875324cf7
Luigi Antonini <luigi.antonini84@gmail.com>
2013-01-09 13:13:54 +0100

    Inserito nuovo curriculum in inglese

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -13,18 +13,18 @@ class FrontController < ApplicationController
 
   def curriculum
 
-    require 'net/http'
-    require 'uri'
-
-    parsed_url = URI.parse('https://docs.google.com/document/pub?id=1b67GGHMnwoEVXUJ2d0E8ETrz-n9PVy9zCdTcnWZdJ2Y')
-    http = Net::HTTP.new(parsed_url.host, parsed_url.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(parsed_url.request_uri)
-    response = http.request(request)
-
-    @curriculum = response.body
-    @curriculum = @curriculum.gsub(/<style.*?>[\s\S]*<\/style>/i, "")
+    # require 'net/http'
+    # require 'uri'
+    #
+    # parsed_url = URI.parse('https://docs.google.com/document/d/192WdGfZ8f-JYKjaITGNgTu_u0w-QvbW5MeESZArOFyw/pub')
+    # http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+    # http.use_ssl = true
+    #
+    # request = Net::HTTP::Get.new(parsed_url.request_uri)
+    # response = http.request(request)
+    #
+    # @curriculum = response.body
+    # @curriculum = @curriculum.gsub(/<style.*?>[\s\S]*<\/style>/i, "")
 
     render :layout => 'clean'
 
